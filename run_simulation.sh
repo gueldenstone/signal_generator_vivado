1bff3a0ba5a728e1426d529d3d1c37d93d8a252c
Lukas Güldenstein <github@lukasgueldenstein.de>
2020-10-27 20:42:33 +0100

    setup new project, changed name

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,19 +1,16 @@
-#!/usr/local/bin/zsh
+#!/usr/bin/zsh
 
 
-cd /Users/lukas/dev/vivado/projects/signal_generator_cmods7
-ghdl -i signal_generator_cmods7.srcs/sources_1/new/signal_generator.vhd \
-signal_generator_cmods7.srcs/sources_1/new/lut.vhd \
-signal_generator_cmods7.srcs/sources_1/new/counter.vhd \
-signal_generator_cmods7.srcs/sources_1/new/clock_divider.vhd \
-signal_generator_cmods7.srcs/sim_1/new/signal_generator_tb.vhd
+# cd /Users/lukas/dev/vivado/projects/signal_generator_vivado
+ghdl -i signal_generator_vivado.srcs/sources_1/imports/new/signal_generator.vhd \
+signal_generator_vivado.srcs/sources_1/imports/new/lut.vhd \
+signal_generator_vivado.srcs/sources_1/imports/new/counter.vhd \
+signal_generator_vivado.srcs/sources_1/imports/new/clock_divider.vhd \
+signal_generator_vivado.srcs/sim_1/imports/new/signal_generator_tb.vhd
 
+echo "Imported all the Files!"
 ghdl -m --ieee=synopsys signal_generator_tb
-# ghdl -a --ieee=synopsys signal_generator_cmods7.srcs/sources_1/new/signal_generator.vhd && \
-# ghdl -a --ieee=synopsys signal_generator_cmods7.srcs/sources_1/new/lut.vhd && \
-# ghdl -a --ieee=synopsys signal_generator_cmods7.srcs/sources_1/new/counter.vhd && \
-# ghdl -a --ieee=synopsys signal_generator_cmods7.srcs/sources_1/new/clock_divider.vhd && \
-# ghdl -a --ieee=synopsys signal_generator_cmods7.srcs/sim_1/new/signal_generator_tb.vhd && \
-# ghdl -e --ieee=synopsys signal_generator_tb && \
+echo "Ran make command"
+
 echo "Running" && \
-ghdl -r --ieee=synopsys signal_generator_tb --vcd=signal_generator_tb.vcd --ieee-asserts=disable-at-0
+ghdl -r --ieee=synopsys signal_generator_tb --vcd=signal_generator_tb.vcd # --ieee-asserts=disable-at-0
---
+++ signal_generator_vivado.srcs/sim_1/imports/new/signal_generator_tb.vhd
@@ -0,0 +1,73 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 10/15/2020 02:25:47 PM
+-- Design Name: 
+-- Module Name: signal_generator_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity signal_generator_tb is
+end signal_generator_tb;
+
+architecture Behavioral of signal_generator_tb is
+    component signal_generator
+    Port 
+    (
+        i_clk_12mhz : in std_logic;
+        o_clk_1mhz : out std_logic;
+        o_dac1_data: out std_logic_vector(11 downto 0)
+    );
+    end component;
+
+    -- test signals
+    signal i_clk_12mhz_sim : std_logic := '0';
+    signal o_clk_1mhz_sim : std_logic := '0';
+    signal o_dac1_data : std_logic_vector(11 downto 0) := (others => '0');
+
+begin
+    testobject : signal_generator
+    port map
+    (
+        i_clk_12mhz => i_clk_12mhz_sim,
+        o_clk_1mhz => o_clk_1mhz_sim,
+        o_dac1_data => o_dac1_data
+    );
+
+    process
+    begin
+        for i in 1 to 100000 loop
+            i_clk_12mhz_sim <= not i_clk_12mhz_sim;
+            wait for 41.66 ns;
+            i_clk_12mhz_sim <= not i_clk_12mhz_sim;
+            wait for 41.66 ns;
+            -- clock period = 83.333 ns = 12Mhz
+          end loop; 
+          wait;
+    end process;
+
+end Behavioral;
